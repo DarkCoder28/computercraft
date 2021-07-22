9ae66cf0db98cf8e6e503d199227461201cb496b
agsheeran <agsheeran@msn.com>
2021-07-21 21:51:56 -0400

    _

--- base_monitor/glasses.lua
+++ base_monitor/glasses.lua
@@ -35,8 +35,8 @@ for i = 1, x do
         end
     end
 end
-canvas.addRectangle(19, y-16, 1, 8, col.teal)
-canvas.addRectangle(20, y-20, 2, 16, col.teal)
+canvas.addRectangle(19, y-19, 1, 14, col.teal)
+canvas.addRectangle(20, y-21, 2, 18, col.teal)
 canvas.addRectangle(22, y-22, 22*4, 20, col.teal)
 os.exit()
 
