2b0b2ad4f3782c76e2023795805bfd92c4e770ec
agsheeran <agsheeran@msn.com>
2021-07-25 16:42:36 -0400

    _

--- autocrafting/assembler.lua
+++ autocrafting/assembler.lua
@@ -1,5 +1,10 @@
+if (fs.exists('/inspect.lua') == false) then
+    shell.run("wget", "https://github.com/kikito/inspect.lua/raw/master/inspect.lua", "inspect.lua")
+end
+inspect = require('/inspect')
+
 chest = peripheral.wrap("front")
 machine = peripheral.wrap("top")
 me = peripheral.wrap("back")
 
-machine.list()
+print(inspect(machine.list()))
