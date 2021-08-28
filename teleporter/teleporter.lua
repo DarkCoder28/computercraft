d5ee9f3b366f815c62a86274264d7ffefc1d3490
agsheeran <agsheeran@msn.com>
2021-08-28 00:16:08 -0400

    _

--- teleporter/teleporter.lua
+++ teleporter/teleporter.lua
@@ -31,7 +31,7 @@ while true do
     term.clear()
     term.setCursorPos(1,1)
     -- Print welcome message in yellow on black
-    term.blit('Welcome to TelOS!\n\n', '444444444444444444ff', 'ffffffffffffffffffff')
+    term.blit('Welcome to TelOS!\n\n', '44444444444444444ff', 'fffffffffffffffffff')
     print('Please enter your destination.\nIf you would like a list of possible destinations, enter \'help\'.\n')
     write('> ')
     local input = read(nil, history, function(text) return auto_complete(text) end)
