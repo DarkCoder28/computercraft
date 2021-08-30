a87bd1bb96785e390276e0dc774f7c54ed318924
agsheeran <agsheeran@msn.com>
2021-08-29 20:55:04 -0400

    _

---
+++ discord_integration/discord_hook.lua
@@ -0,0 +1,15 @@
+chat = peripheral.wrap('left')
+connection_url = 'ws://192.168.1.64:3002'
+local ws, err = http.websocket(connection_url)
+
+if not ws then
+    os.reboot()
+end
+ws.send('modded-chat')
+
+while true do
+    local _, url, response, isBinary = os.pullEvent("websocket_message")
+    if (url == connection_url and isBinary == false) then
+        print(response)
+    end
+end
