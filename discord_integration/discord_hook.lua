4a84928bc50e7acdde3d5dd5d9e5683d6d4aa930
agsheeran <agsheeran@msn.com>
2021-09-10 23:46:21 -0400

    _

--- discord_integration/discord_hook.lua
+++ discord_integration/discord_hook.lua
@@ -20,13 +20,28 @@ function len(str)
     return #str
 end
 
+function chat(sender, msg)
+    local maxLen = 77
+    local prefixLen = len(sender)+3
+    local messageLen = len(msg)-prefixLen
+    if messageLen > maxLen then
+        chat.say('<'..sender..'> '..msg:sub(1,maxLen-prefixLen))
+        for i = maxLen-prefixLen-22, messageLen, maxLen do
+            chat.say(msg:sub(i,i+maxLen))
+        end
+    else
+        chat.say('<'..sender..'> '..msg)
+    end
+end
+
 local function receiver()
     local _, url, response, isBinary = os.pullEvent("websocket_message")
     if (url == connection_url and isBinary == false) then
         data = split(response)
         data[1] = data[1]:sub(1,data[1]:len()-1)
         data[2] = data[2]:sub(2)
-        chat.say('<'..data[1]..'> '..data[2])
+        --chat.say('<'..data[1]..'> '..data[2])
+        chat(data[1], data[2])
     end
 end
 
@@ -34,17 +49,7 @@ chat.capture('')
 local function transmitter()
     while true do
         local _, message, _, player = os.pullEvent('chat_capture')
-        local maxLen = 77
-        local prefixLen = len(player)+3
-        local messageLen = len(message)-prefixLen
-        if messageLen > maxLen then
-            chat.say('<'..player..'> '..message:sub(1,maxLen-prefixLen))
-            for i = maxLen-prefixLen-22, messageLen, maxLen do
-                chat.say(message:sub(i,i+maxLen))
-            end
-        else
-            chat.say('<'..player..'> '..message)
-        end
+        chat(player, message)
         if not pcall(ws.send, player..' |:| '..message) then
             os.reboot()
         end
