d06c17feac45dc91ba3f9a24ad17833591076c7c
agsheeran <agsheeran@msn.com>
2022-01-30 13:44:29 -0500

    .

--- farming/melon.lua
+++ farming/melon.lua
@@ -1,17 +1,33 @@
 local pretty = require "cc.pretty"
 
+function turnAround()
+    turtle.turnRight()
+    turtle.turnRight()
+end
+
 while true do
     local blockDown, down = turtle.inspectDown()
     local blockFront, front = turtle.inspect()
-    print(down.name)
     if (blockFront) then
-        if front.name == 'oak_planks' then
+        if front.name == 'minecraft:oak_planks' then
             turtle.turnRight()
         end
     end
     if blockDown then
-        if down.name == '' then
-            --
+        if down.name == 'minecraft:chest' then
+            turnAround()
+            for i = 1, 16, 1 do
+                turtle.select(i)
+                turtle.dropDown()
+            end
+        elseif down.name == 'minecraft:melon' then
+            turtle.digDown()
+            turtle.suckDown()
+        elseif down.name == 'minecraft:cobblestone' or down.name == 'minecraft:sand' then
+            turtle.turnLeft()
+        elseif down.name == 'minecraft:oak_planks' then
+            turnAround()
         end
     end
+    turtle.forward()
 end
