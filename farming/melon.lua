f530f44fd131b58c7091fc6234a359428666b792
agsheeran <agsheeran@msn.com>
2022-01-30 13:38:32 -0500

    .

--- farming/melon.lua
+++ farming/melon.lua
@@ -1,8 +1,17 @@
 local pretty = require "cc.pretty"
 
---while true do
+while true do
     local blockDown, down = turtle.inspectDown()
-    pretty.pretty_print(down)
     local blockFront, front = turtle.inspect()
-    pretty.pretty_print(front)
---end
+    print(down.name)
+    if (blockFront) then
+        if front.name == 'oak_planks' then
+            turtle.turnRight()
+        end
+    end
+    if blockDown then
+        if down.name == '' then
+            --
+        end
+    end
+end
