8d2fd3cc1953c2223ea16095fbd390d9b7a535a0
agsheeran <agsheeran@msn.com>
2022-01-30 13:50:23 -0500

    .

--- farming/melon.lua
+++ farming/melon.lua
@@ -8,14 +8,16 @@ end
 while true do
     local blockDown, down = turtle.inspectDown()
     local blockFront, front = turtle.inspect()
-    if (blockFront) then
+    if blockFront then
         if front.name == 'minecraft:oak_planks' then
             turtle.turnRight()
         end
     end
     if blockDown then
         if down.name == 'minecraft:chest' then
-            turnAround()
+            if blockFront and front.name == 'minecraft:sand' then
+                turnAround()
+            end
             for i = 1, 16, 1 do
                 turtle.select(i)
                 turtle.dropDown()
@@ -23,10 +25,14 @@ while true do
         elseif down.name == 'minecraft:melon' then
             turtle.digDown()
             turtle.suckDown()
-        elseif down.name == 'minecraft:cobblestone' or down.name == 'minecraft:sand' then
+        elseif down.name == 'minecraft:cobblestone' then
             turtle.turnLeft()
+        elseif down.name == 'minecraft:sand' then
+            turtle.turnRight()
         elseif down.name == 'minecraft:oak_planks' then
-            turnAround()
+            if blockFront and front.name == 'minecraft:cobblestone' then
+                turnAround()
+            end
         end
     end
     turtle.forward()
