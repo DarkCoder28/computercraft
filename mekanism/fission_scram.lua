3970a01a31471154bbf0a7e8c410d0b84ee56ce9
agsheeran <agsheeran@msn.com>
2022-02-08 22:28:46 -0500

    .

--- mekanism/fission_scram.lua
+++ mekanism/fission_scram.lua
@@ -1,4 +1,5 @@
 local reactor = peripheral.find("fissionReactorLogicAdapter")
+print("Fission Reactor Protection Active!")
 while true do
     if reactor.getDamagePercent() > 0 or reactor.getCoolantFilledPercentage() < 0.75 then
         if reactor.getStatus() then
