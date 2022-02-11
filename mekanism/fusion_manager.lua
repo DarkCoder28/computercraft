d4e9e57236401b1597b7f432263f56d5abf03d22
agsheeran <agsheeran@msn.com>
2022-02-10 20:02:57 -0500

    .

--- mekanism/fusion_manager.lua
+++ mekanism/fusion_manager.lua
@@ -11,7 +11,7 @@ print("Fusion Reactor Management Active!")
 while true do
     if reactor.getCaseTemperature() >= 200000000 or reactor.getPlasmaTemperature() >= 350000000 then
         reactor.setInjectionRate(2)
-    elseif reactor.getCaseTemperature <= 75000000 or reactor.getPlasmaTemperature <= 200000000 then
+    elseif reactor.getCaseTemperature() <= 75000000 or reactor.getPlasmaTemperature() <= 200000000 then
         reactor.setInjectionRate(4)
     end
 end
