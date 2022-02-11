2990d894335cd7366c5d17a5ffc6183cca1d78bc
agsheeran <agsheeran@msn.com>
2022-02-10 20:04:21 -0500

    .

--- mekanism/fusion_manager.lua
+++ mekanism/fusion_manager.lua
@@ -1,14 +1,20 @@
 local reactor = peripheral.find("fusionReactorLogicAdapter")
 
 reactor.setInjectionRate(2)
-term.clear()
-term.setCursorPos(1,1)
-term.setTextColour(colours.yellow)
-print("ReactOS v1.1")
-term.setTextColour(colours.white)
-print("Fusion Reactor Management Active!")
+function clearTerm()
+    term.clear()
+    term.setCursorPos(1,1)
+    term.setTextColour(colours.yellow)
+    print("ReactOS v1.1")
+    term.setTextColour(colours.white)
+    print("Fusion Reactor Management Active!")
+end
+clearTerm()
 
 while true do
+    clearTerm()
+    print(reactor.getCaseTemperature())
+    print(reactor.getPlasmaTemperature())
     if reactor.getCaseTemperature() >= 200000000 or reactor.getPlasmaTemperature() >= 350000000 then
         reactor.setInjectionRate(2)
     elseif reactor.getCaseTemperature() <= 75000000 or reactor.getPlasmaTemperature() <= 200000000 then
